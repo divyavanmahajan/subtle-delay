ebc12d8f4f2a1ebc156fb10bca90bc3c21cc9445
Appuser <divya@users.noreply.github.com>
2016-05-13 11:39:30 -0400

    Push metrics to Loggly. Add dev/prod to TWilio messages.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ monitor.util=require('./lib/util.js');
 monitor.sfquery=require('./lib/sfquery.js');
 monitor.dbquery=require('./lib/dbquery.js');
 monitor.sendalert=require('./lib/sendalert.js');
-
+const w=monitor.util.winston;
 /**
  * Setup the configuration for the program usernames, passwords etc.
  * This will initialize the module and must be called before using the other functions.
@@ -26,7 +26,7 @@ monitor.initialize = function(dbconfig) {
             monitor.config = dbconfig;
         }
         
-        //console.log(JSON.stringify(monitor.config));
+        //w.debug(JSON.stringify(monitor.config));
         process.on('SIGINT', onCtrlC);
         monitor.util.initialize(monitor.config);
         monitor.sfquery.initialize(monitor.config);
@@ -68,32 +68,31 @@ function poll(timestamp,table,objectname) {
     var sfchanges;
     monitor.dbquery.query_staging_table(table, 'DB_'+objectname,timestamp).then(function(res) {
         if (typeof(res)=="undefined") {
-		console.error(moment().format()+":Error during query");
+		w.error(moment().format()+":Error during query");
 	}
 	var tablename=res.tablename;
 	var query = res.query;
 	var metadata = res.metadata;
 	var rows = res.rows;
         dbchanges = rows;
-        //console.log('dbquery:query_all_staging - returns :' + tablename + ":" + rows.length);
-        //console.log(metadata);
-        //console.log(rows);
+        //w.debug('dbquery:query_all_staging - returns :' + tablename + ":" + rows.length);
+        //w.debug(metadata);
+        //w.debug(rows);
 
         monitor.sfquery.getObjectsChanged(objectname,'SF_'+objectname, timestamp, function(err,sf_object, object_name, for_time, query, result) {
-            // console.log('sfquery:getObjectsChanged returns');
+            // w.debug('sfquery:getObjectsChanged returns');
             sfchanges = result;
 	    if (err==null) {
-		//console.log('Salesforce returns : ' + result.totalSize);
+		//w.debug('Salesforce returns : ' + result.totalSize);
 		compareChanges(timestamp,dbchanges,sfchanges,sf_object);
 	    } else {
-		//console.log('Salesforce returns : '+ err);
+		//w.debug('Salesforce returns : '+ err);
 		compareChanges(timestamp,dbchanges,null,sf_object);
 	    }
           }
         );
     },function(err) {
-        console.error(moment().format()+": Error during polling:"+JSON.stringify(err));
-        console.log(moment().format()+": Error during polling:"+JSON.stringify(err));
+        w.error(moment().format()+": Error during polling:"+JSON.stringify(err));
     });
     
 }
@@ -107,22 +106,22 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
     var sf_missed = [];
     var late = [];
     var okay = [];
-    console.log(moment().format()+":"+'CompareChanges for '+objectname);
+    w.info(moment().format()+":"+'CompareChanges for '+objectname);
 
     if (sfchanges==null) {
-        console.log(moment().format()+":"+'   No data from Salesforce');
+        w.info(moment().format()+":"+'   No data from Salesforce');
     } else {
-        console.log(moment().format()+":"+'   Salesforce '+sfchanges.records.length+' records for '+objectname);
+        w.info(moment().format()+":"+'   Salesforce '+sfchanges.records.length+' records for '+objectname);
         for (var i = 0; i < sfchanges.records.length; i++) {
             var record = sfchanges.records[i];
-            //console.log(JSON.stringify(record));
+            //w.debug(JSON.stringify(record));
             var timestring = moment(record.LastModifiedDate).utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
-            console.log(moment().format()+":"+"     "+record.Id+":"+timestring);
+            w.info(moment().format()+":"+"     "+record.Id+":"+timestring);
             map[record.Id] = record.LastModifiedDate;
         }
     }
 
-    console.log(moment().format()+":"+'   Database '+dbchanges.length+' records for '+objectname);
+    w.info(moment().format()+":"+'   Database '+dbchanges.length+' records for '+objectname);
     var total_latency = 0;
     for (var j=0;j<dbchanges.length;j++) {
         var sf_id = dbchanges[j][0];
@@ -137,7 +136,7 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
 	var latency = (db_date.toDate() - sf_date.toDate())/1000;
 		total_latency = total_latency + latency;
 	var MAX_SLA_LATENCY=300; // 300s = 5 minutes
-        console.log(moment().format()+":"+'     '+sf_id+":"+db_lastmodified+"  "+db_updatedTime+"  "+latency);
+        w.info(moment().format()+":"+'     '+sf_id+":"+db_lastmodified+"  "+db_updatedTime+"  "+latency);
         
         var value = map[sf_id];
 	var fbrecord = {'sf_id':sf_id,'db_lastmodified':db_lastmodified,
@@ -145,13 +144,13 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
 
 	// Remove this record id+lastmodified from the missing global list
 	try {
-	// console.log('Missed:'+sf_id+' - '+db_lastmodified);
+	// w.debug('Missed:'+sf_id+' - '+db_lastmodified);
 	var removekey="missed/"+objectname+'/'+sf_id+"/"+db_lastmodified;
 	monitor.util.removeFirebaseString(removekey);
-	} catch (err101) { console.log(moment().format()+":"+"   Error removing "+removekey+":"+err101);}
+	} catch (err101) { w.error(moment().format()+":"+"   Error removing "+removekey+":"+err101);}
 
         if (typeof(value)=='undefined') {
-            console.log(moment().format()+":"+'  Previous DB batch?: '+sf_id+":"+value);
+            w.info(moment().format()+":"+'  Previous DB batch?: '+sf_id+":"+value);
             // Database result was is not in Salesforce query. 
             // It is probably in the previous SF query window.
         } else {
@@ -162,23 +161,23 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
             late.push(fbrecord);
 	    try {
 	    monitor.util.updateFirebaseString("late/"+objectname+'/'+sf_id,db_lastmodified,latency);
-	    } catch (err102) { console.log(moment().format()+":"+"   Error updating late: "+sf_id+":"+err102);}
+	    } catch (err102) { w.error(moment().format()+":"+"   Error updating late: "+sf_id+":"+err102);}
 	} else {
             
             if (sf_lastmodified> db_lastmodified) {
-                console.log(moment().format()+":"+'  Multiple updates. Latest missed: '+sf_id+": Salesforce - "+sf_lastmodified+" | DB - "+lastmodifieddate);
+                w.info(moment().format()+":"+'  Multiple updates. Latest missed: '+sf_id+": Salesforce - "+sf_lastmodified+" | DB - "+lastmodifieddate);
                 // This update has not come through but an earlier update came in the same window.
                 sf_missed.push({'sf_id':sf_id,'sf_lastmodified':sf_lastmodified });
  		try {
 		monitor.util.updateFirebaseString("missed/"+objectname+'/'+sf_id,sf_lastmodified,1);
-	        } catch (err103) { console.log(moment().format()+":"+"   Error updating missed "+id+":"+err103);}
+	        } catch (err103) { w.error(moment().format()+":"+"   Error updating missed "+id+":"+err103);}
             } else {
 		// okay.push({'sf_id':sf_id,'sf_lastmodified':sf_lastmodified, 'db_updatedTime':db_updatedTime,'latency':latency });
-		//console.log(":::sf_id:"+sf_id+"  fbrecord:"+fbrecord.sf_id);
+		//w.debug(":::sf_id:"+sf_id+"  fbrecord:"+fbrecord.sf_id);
 		okay.push(fbrecord);
  		try {
 		monitor.util.updateFirebaseString("okay/"+objectname+'/'+sf_id,sf_lastmodified,latency);
-	        } catch (err103) { console.log(moment().format()+":"+"   Error updating okay "+sf_id+":"+err103);}
+	        } catch (err103) { w.error(moment().format()+":"+"   Error updating okay "+sf_id+":"+err103);}
             }
             delete map[sf_id];
         }
@@ -190,7 +189,7 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
         sf_missed.push({'sf_id':id,'sf_lastmodified':timestring });
  	try {
 	monitor.util.updateFirebaseString("missed/"+objectname+'/'+id,timestring,1);
-        } catch (err104) { console.log(moment().format()+":"+"   Error updating missed "+id+":"+err104);}
+        } catch (err104) { w.error(moment().format()+":"+"   Error updating missed "+id+":"+err104);}
     }
 
     var average_latency=0;
@@ -202,9 +201,18 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
 			+'| ok:'+okay.length+" missed:"+sf_missed.length+" late:"+late.length+' latency:'+average_latency;
         monitor.sendalert.alert(message);
     }
-    console.log(moment().format()+":"+'   Metrics: '+objectname+' |'+timestamp.format('YYYY-MM-DDTHH:mm:ss[Z]')
+    var metrics={};
+    metrics["type"]="Metrics";
+    metrics["timestamp"]=timestamp.format('YYYY-MM-DDTHH:mm:ss[Z]');
+    metrics["object"]=objectname;
+    metrics["okay"]=okay.length;
+    metrics["missed"]=missed.length;
+    metrics["late"]=late.length;
+    metrics["latency"]=average_latency;
+    monitor.util.logglyclient.log(metrics);
+    w.info(moment().format()+":"+'   Metrics: '+objectname+' |'+timestamp.format('YYYY-MM-DDTHH:mm:ss[Z]')
 			+'| okay:'+okay.length+" missed:"+sf_missed.length+" late:"+late.length+' latency:'+average_latency);
-    console.log(moment().format()+":"+'   Missed:',sf_missed);
+    w.info(moment().format()+":"+'   Missed:',sf_missed);
     
     monitor.util.updateFirebase(timestamp, objectname+'_latency', average_latency);
     monitor.util.updateFirebase(timestamp, objectname+'_late', late.length);
@@ -212,7 +220,7 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
     monitor.util.updateFirebase(timestamp, objectname+'_missed', sf_missed.length);
     monitor.util.updateFirebase(timestamp, objectname+'_missed_records', sf_missed);
     monitor.util.updateFirebase(timestamp, objectname+'_late_records', late);
-    console.log(moment().format()+":"+'Finished compare.');
+    w.info(moment().format()+":"+'Finished compare.');
 
 }
 
@@ -220,7 +228,7 @@ function compareChanges(timestamp,dbchanges, sfchanges,objectname) {
  * Internal: Function to trap Ctrl+C and exit the process. 
  */
 function onCtrlC() {
-    console.log("\nGracefully shutting down from SIGINT (Ctrl+C)");
+    w.info("\nGracefully shutting down from SIGINT (Ctrl+C)");
     setTimeout(process.exit, 2000); // Wait 2 seconds and exit. This is needed for Firebase to exit.
 }
 
